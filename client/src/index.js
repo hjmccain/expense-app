b8a4e63acc57ebefa632347fdf271389aaec5696
hjmccain <hannahjmccain@gmail.com>
2018-04-30 14:18:42 -0400

    Add app root.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './app/App';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
