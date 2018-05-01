8bc73d9799e207ab636b19a0fb478767c9dfdc87
hjmccain <hannahjmccain@gmail.com>
2018-05-01 13:20:09 -0400

    Update package.json for Sass.

--- client/src/app/header.js
+++ client/src/app/header.js
@@ -3,8 +3,10 @@ import React from 'react';
 const AppHeader = props => {
   return (
     <div className="app-header">
-      <h3>Welcome!</h3>
-      <div>[profile]</div>
+      <h3 className="app-header__element -welcome">Welcome!</h3>
+      <div classname="app-header__element">
+        <a href="#">My profile</a>
+      </div>
     </div>
   );
 };
