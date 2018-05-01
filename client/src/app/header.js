cf099d2554f293f8c549e724ebed6ba118f4b1b6
hjmccain <hannahjmccain@gmail.com>
2018-05-01 14:59:26 -0400

    Add flexbox and css-grid to app styling.

--- client/src/app/header.js
+++ client/src/app/header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AppHeader = props => {
   return (
-    <div className="app-header">
-      <h3 className="app-header__element -welcome">Welcome!</h3>
-      <div className="app-header__element">
+    <div className="app-container__header">
+      <h3 className="app-container__header__element -welcome">Welcome!</h3>
+      <div className="app-container__header__element">
         <a href="google.com">My profile</a>
       </div>
     </div>
