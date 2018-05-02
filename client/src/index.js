241c592772eb84d14a206cfa59b39ea03c1b027c
hjmccain <hannahjmccain@gmail.com>
2018-05-02 14:41:44 -0400

    Add Apollo to React.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
-import './style/index.css'
+import './style/index.css';
+
+import ApolloClient from 'apollo-boost';
+import gql from "graphql-tag";
+
+// defaults to /graphql on app host if no uri object is passed
+const client = new ApolloClient({
+  uri: `http:localhost:5000/graphql`
+});
+
+client.query({
+  query: gql`
+    {
+      hello
+    }
+  `
+}).then(res => console.log(res));
 
 ReactDOM.render(
   <App />,
