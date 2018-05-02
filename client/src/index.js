11d1abc8621d1dbb166feb79dadea14e645014d2
hjmccain <hannahjmccain@gmail.com>
2018-05-02 15:46:48 -0400

    Remove all Apollo/GraphQL stuff.

--- client/src/index.js
+++ client/src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './app/App';
 import './style/index.css';
 
-import ApolloClient from 'apollo-boost';
-import gql from "graphql-tag";
-
-// defaults to /graphql on app host if no uri object is passed
-const client = new ApolloClient({
-  uri: `http:localhost:5000/graphql`
-});
-
-client.query({
-  query: gql`
-    {
-      hello
-    }
-  `
-}).then(res => console.log(res));
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
--- index.js
+++ index.js
@@ -3,33 +3,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools'); 
-
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: { hello: () => "hello world!" }
-};
-
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Add a 'catchall' handler, so that for any requests that don't match the one(s) outlined
 // above, React's index.html file will be sent back.
