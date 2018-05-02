4e7a048f369c134c47f7aa72621133aa621c36b7
hjmccain <hannahjmccain@gmail.com>
2018-05-02 14:47:19 -0400

    Update backend to use Apollo.

--- index.js
+++ index.js
@@ -3,29 +3,46 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-var graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql'); 
+// var graphqlHTTP = require('express-graphql');
+// const { buildSchema } = require('graphql');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools'); 
 
-const schema = buildSchema(`
+const typeDefs = `
   type Query {
     hello: String
   }
-`)
+`;
 
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  }
-}
+const resolvers = {
+  Query: { hello: () => hello }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+
+// const root = {
+//   hello: () => {
+//     return 'Hello world!';
+//   }
+// }
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true
+// }));
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 // Add a 'catchall' handler, so that for any requests that don't match the one(s) outlined
 // above, React's index.html file will be sent back.
