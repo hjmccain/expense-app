f42a30ce364341c5e790cc113b4829a7dea44af4
hjmccain <hannahjmccain@gmail.com>
2018-05-08 12:36:24 -0400

    Add resolvers & db for mock data.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const resolvers = {
+const expenses = [
+  { id: 1,
+    total: 202,
+    description: 'Amazon-Rinck Prime Account'
+  },
+  { id: 2,
+    total: 56,
+    description: 'MikalaDavis'
+  },
+  { id: 3,
+    total: 68,
+    description: 'LEWISTON*USA*Orchid'
+  },
+  { id: 4,
+    total: 85,
+    description: 'Shaws-Supermarkets'
+  }
+]
 
-}
+const resolvers = {
+  Query: {
+    test: () => 'This is a test',
+    expenses: () => expenses
+  },
+  Expense: {
+    id: root => root.id,
+    total: root => root.total,
+    description: root => root.description,
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/scheme.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
